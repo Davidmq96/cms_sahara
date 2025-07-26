8262890f8fdcc0e825221cd087e90972de29f16c
davidmq096 <david.medina@inceptio.com.mx>
2025-07-25 18:19:17 -0600

    este es el bueno

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { createDirectus } from 'directus/dist/index.js';
 import config from './directus.config.js';
 
-const directus = await createDirectus(config);
+const directus = await createDirectus(config);//indexs
 await directus.start();
