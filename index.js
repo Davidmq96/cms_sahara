87bb77cc4cd9d2db1f22269049e207aeec65ca05
davidmq096 <david.medina@inceptio.com.mx>
2025-07-26 07:50:52 -0600

    test

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-import { createDirectus } from 'directus/dist/index.js';
-import config from './directus.config.js';
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const { spawn } = require('child_process');
 
-const directus = await createDirectus(config);//indexs
-await directus.start();
+const app = express();
+
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+
+app.use('/api', createProxyMiddleware({
+  target: 'https://cms-sahara.onrender.com',
+  changeOrigin: true,
+  pathRewrite: { '^/api': '' },
+}));
+
+
+
+// Lanzar Directus como proceso hijo
+const directusProcess = spawn('npx', ['directus', 'start'], { stdio: 'inherit' });
+
+directusProcess.on('close', (code) => {
+  console.log(`Directus proceso terminó con código ${code}`);
+});
